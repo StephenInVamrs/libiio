f67867bda734e2e95eb923929e817f1e05da868b
Paul Cercueil <paul@crapouillou.net>
2021-04-13 11:30:10 +0100

    local: Allow creating context with params
    The params are currently unused, but this will be helpful in the future
    when configuring e.g. the log level at context creation time.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- iio-private.h
+++ iio-private.h
@@ -251,7 +251,7 @@ int iio_device_get_poll_fd(const struct iio_device *dev);
 int read_double(const char *str, double *val);
 int write_double(char *buf, size_t len, double val);
 
-struct iio_context * local_create_context(void);
+struct iio_context * local_create_context(const struct iio_context_params *params);
 struct iio_context * network_create_context(const char *hostname);
 struct iio_context * xml_create_context_mem(const struct iio_context_params *params,
 					    const char *xml, size_t len);
